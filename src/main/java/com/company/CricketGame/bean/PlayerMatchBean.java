e5c711b1c6574fedd2b4707ba93c286fda498727
Ujjwal Goel <ujjwalgoel113@gmail.com>
2022-03-04 19:27:42 +0530

    pr done

--- src/main/java/com/company/CricketGame/bean/PlayerMatchBean.java
+++ src/main/java/com/company/CricketGame/bean/PlayerMatchBean.java
@@ -15,6 +15,7 @@ public class PlayerMatchBean {
         this.teamId = teamId;
         this.score = score;
         this.numberOfBallsPlayed = numberOfBallsPlayed;
+        ef;
     }
 
     public int getPlayerId() {
